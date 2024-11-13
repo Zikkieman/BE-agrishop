39ba28df9904f43be68079570d7d17a37b6d7fee
Zikkieman <horlarmeydeileh50@gmail.com>
2024-11-13 21:23:41 +0100

    still testing with wild card

--- server.js
+++ server.js
@@ -18,21 +18,17 @@ const app = express();
 const port = 5000;
 app.options("*", cors());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://agrishop-five.vercel.app",
-];
+const allowedOrigins = ["*"];
 
 app.use(
   cors({
-    // origin: function (origin, callback) {
-    //   if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error("Not allowed by CORS"));
-    //   }
-    // },
-    allowedOrigins: ["*"],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
